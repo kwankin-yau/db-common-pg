29a2e5b96f34c7973fa40f0dedca5f4df9836de5
KwanKin Yau <alphax@vip.163.com>
2021-11-06 11:45:28 +0800

    # 1.0.0 - [2021-10-30] initial commit

---
+++ src/main/scala/com/lucendar/common/db/pg/SqlDialect_Pg.scala
@@ -0,0 +1,26 @@
+/** *****************************************************************************
+ * Copyright (c) 2019, 2021 lucendar.com.
+ * All rights reserved.
+ *
+ * Contributors:
+ * KwanKin Yau (alphax@vip.163.com) - initial API and implementation
+ * ***************************************************************************** */
+package com.lucendar.common.db.pg
+
+import com.lucendar.common.db.types.{SqlDialect, SqlDialects}
+
+import java.sql.Connection
+import scala.util.Using
+
+object SqlDialect_Pg extends SqlDialect {
+  override def id: String = SqlDialects.POSTGRESQL
+
+  override def stringValueLiteral(s: String): String =
+    org.postgresql.core.Utils.escapeLiteral(null, s, true).toString
+
+  override def setConstraintsDeferred(conn: Connection): Unit = {
+    Using.resource(conn.createStatement()) { st =>
+      st.execute("SET CONSTRAINTS ALL DEFERRED")
+    }
+  }
+}
